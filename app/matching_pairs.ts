882a672d127cfd3dbd58da5ed99bbea581a9fc70
billywestbury <billy.westbury@outlook.com>
2016-01-31 17:33:39 +0000

    looking good!

--- app/matching_pairs.ts
+++ app/matching_pairs.ts
@@ -55,12 +55,6 @@ export class MatchingPairs {
     shuffledList: Array<any> = [];
     masterCounter: number = 0;
     flipedTileCounter: number = 0;
-    square1Num: any;
-    square2Num: any;
-    savedSquareX1: any;
-    savedSquareY1: any;
-    savedSquareX2: any;
-    savedSquareY2: any;
     tileset: any;
     marker: any;
     currentMarker: ITile;
@@ -75,6 +69,9 @@ export class MatchingPairs {
     currentTiles: Array<any>;
     tileGrid: Array<any>;
     tilesList: Array<ITileObj>;
+
+    levelArray: Array<any>;
+    shuffledLevelArray: Array<any>;
 
     constructor() {
         this.game = new Phaser.Game(900, 600, Phaser.AUTO, 'content', {
@@ -92,9 +89,11 @@ export class MatchingPairs {
 
     // start of create
     create() {
-        this.tilesList = [];
+        this.levelArray = [];
+        this.shuffledLevelArray = [];
         this.game.input.mouse.capture = true;
         this.game.input.onTap.add(MatchingPairs.prototype.onTap, this);
+
         this.map = this.game.add.tilemap('matching');
         this.map.addTilesetImage('Desert', 'tiles');
         this.layer = this.map.createLayer('Ground');
@@ -103,34 +102,40 @@ export class MatchingPairs {
         this.marker.lineStyle(2, 0x00FF00, 1);
         this.marker.drawRect(0, 0, 100, 100);
 
-        // this.tileGrid = [
-        //     [tilesList[1], tilesList[2], tilesList[3], tilesList[4], tilesList[5], tilesList[6]],
-        //     [tilesList[7], tilesList[8], tilesList[9], tilesList[10], tilesList[11], tilesList[12]],
-        //     [tilesList[13], tilesList[14], tilesList[15], tilesList[16], tilesList[17], tilesList[18]],
-        //     [tilesList[19], tilesList[20], tilesList[21], tilesList[22], tilesList[23], tilesList[24]],
-        //     [tilesList[25], tilesList[26], tilesList[27], tilesList[28], tilesList[29], tilesList[30]],
-        //     [tilesList[31], tilesList[32], tilesList[33], tilesList[34], tilesList[35], tilesList[36]]
-        // ];
-
-        for (let col = 0; col < 6; col++) {
-            for (let row = 0; row < 6; row++) {
-                let tile = this.map.putTile(36, col, row);
-                let tmpTileObj: ITileObj = {
-                    tileRef: 0,
-                    id: 0,
-                    x: tile.x,
-                    y: tile.y,
-                    isFliped: false,
-                    tilePosition: 0,
-                    isMatched: false,
-                    tile: tile,
-                    coverTileIndex: tile.index
-                };
-                this.tilesList.push(tmpTileObj);
-            }
+        for (let num = 1; num <= 18; num++) {
+            this.levelArray.push(num);
+        }
+        for (let num = 1; num <= 18; num++) {
+            this.levelArray.push(num);
         }
 
-        console.log('tileList: ', this.tilesList);
+
+        this.shuffledLevelArray = Phaser.ArrayUtils.shuffle(this.levelArray);
+        console.log('shuffledLevelArray: ', this.shuffledLevelArray);
+        // for (let i = 1; i <= 36; i++) {
+        //     let randomPosition = this.game.rnd.integerInRange(0, this.startList.length - 1);
+
+        //     let thisNumber = this.startList[randomPosition];
+        //     currentLevelArray.push(thisNumber);
+        //     let a = this.startList.indexOf(thisNumber);
+        //     this.startList.splice(a, 1);
+        // }
+        // let startList: Array<any> = [];
+        // for (let num = 1; num <= 18; num++) {
+        //     startList.push(num);
+        // }
+        // for (let num = 1; num <= 18; num++) {
+        //     startList.push(num);
+        // }
+
+        // this.shuffledList = Phaser.ArrayUtils.shuffle(this.startList);
+
+        // put tile cover in place
+        for (let col = 0; col < 6; col++) {
+            for (let row = 0; row < 6; row++) {
+                this.map.putTile(36, col, row);
+            }
+        }
 
     };
     // end of create
@@ -148,71 +153,58 @@ export class MatchingPairs {
     }
 
     onTap(pointer: any, tap: any) {
-        let x = this.layer.getTileX(this.game.input.activePointer.worldX) * 100;
-        console.log('x: ', x);
-        let y = this.layer.getTileY(this.game.input.activePointer.worldY) * 100;
-        console.log('y: ', y);
-        let currentTilePosition = ((this.layer.getTileY(this.game.input.activePointer.worldY) + 1) * 6) - (6 - (this.layer.getTileX(this.game.input.activePointer.worldX) + 1));
-        console.log('currentTilePosition: ', currentTilePosition);
-        console.log('Hidden Tile: ', this.tilesList[currentTilePosition]);
-        // this.currentTile = {
-        //     x: x,
-        //     y: y,
-        //     isFliped: true,
-        //     tilePosition: currentTilePosition
-        // };
+        let tappedTtileX = this.layer.getTileX(this.marker.x);
+        let tappedTtileY = this.layer.getTileY(this.marker.y);
 
-        // console.log('this.currentTile', this.currentTile);
+        let tappedPosition = ((this.layer.getTileY(this.game.input.activePointer.worldY) + 1) * 6) - (6 - (this.layer.getTileX(this.game.input.activePointer.worldX) + 1));
+        console.log('tapPosition: ', tappedPosition);
 
-        // this.currentTurn.tileCounter++;
+        let hiddenTileId = this.shuffledLevelArray[tappedPosition - 1];
+        console.log('hiddenTileId: ', hiddenTileId);
 
-        // if (this.currentTurn.tileCounter === 1) {
-        //     this.currentTurn.firstTile = this.currentTile;
-        // }
+        this.currentTile = this.map.getTile(tappedTtileX, tappedTtileY);
+        console.log('Hidden Tile: ', this.currentTile);
 
-        // if (this.currentTurn.tileCounter === 2) {
-        //     this.turnCounter++;
-        //     this.currentTurn.secondTile = this.currentTile;
-        // }
-
-        // this.currentTile = {
-        //     x: 0,
-        //     y: 0,
-        //     isFliped: false,
-        //     tilePosition: 0
-        // };
+        this.map.putTile(hiddenTileId, tappedTtileX, tappedTtileY);
+        // tileXY.alpha = 0.5;
+        // this.layer.dirty = true;
     };
 
 } // end of class
 
-
-
-// function setCurrentMarker(marker: ITile) {
-//     this.currentTile = {
-//         x: marker.x,
-//         y: marker.y,
-//         isFliped: marker.isFliped
-//     };
-//     console.debug('setCurrentMarker: ', this.currentMarker);
-// }
-
-function RandomizeTiles() {
-    let startList: Array<any> = [];
-    for (let num = 1; num <= 18; num++) {
-        startList.push(num);
-    }
-    for (let num = 1; num <= 18; num++) {
-        startList.push(num);
-    }
-    return startList;
-}
-function flipOver() {
+function flipOver(hiddenTileId: number, tappedTtileX: number, tappedTtileY: number) {
     console.log('flipOver');
-    this.map.putTile(this.currentNum, this.layer.getTileX(this.marker.x), this.layer.getTileY(this.marker.y));
+    this.map.putTile(hiddenTileId, tappedTtileX, tappedTtileY);
 }
+
 function flipBack() {
     console.log('flipOver');
     this.flipFlag = false;
     this.map.putTile(this.tileBack, this.savedSquareX1, this.savedSquareY1);
     this.map.putTile(this.tileBack, this.savedSquareX2, this.savedSquareY2);
 }
+
+// function changeLayer(key: any) {
+//     switch (key.keyCode)
+//     {
+//         case Phaser.Keyboard.ONE:
+//             currentLayer = layer1;
+//             layer1.alpha = 1;
+//             layer2.alpha = 0.2;
+//             layer3.alpha = 0.2;
+//             break;
+
+//         case Phaser.Keyboard.TWO:
+//             currentLayer = layer2;
+//             layer1.alpha = 0.2;
+//             layer2.alpha = 1;
+//             layer3.alpha = 0.2;
+//             break;
+// default:
+//             currentLayer = layer2;
+//             layer1.alpha = 0.2;
+//             layer2.alpha = 1;
+//             break;
+//     }
+
+// }
